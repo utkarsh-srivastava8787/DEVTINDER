3f5ef464695c405f04fecbce1dd15560a48b65dc
utkarsh <utkarshsrivastava@8787@gmail.com>
2026-07-03 22:27:50 +0530

    back

--- README.md
+++ README.md
@@ -1,10 +1,36 @@
--instalize git
+-initialize git
+
 -git ignore
+
+-Query Parameters:
+                Query parameters are key-value pairs that appear after the ? in a URL.
+                http://example.com/search?q=express&page=2
+                In this URL, q=express and page=2 are query parameters that can be accessed as "req.query.q" and "req.query.page"
+
 -create routes 
     -Optional Parameters ('/users/:id?') => Matches:  /users,/users/101(id are option work for both )
+                In this example, id are route parameters that
+                 can be accessed via req.params.
+
     -Regular Expressions (/^\/user\/[0-9]+$/) => /user/123
     -String paths ('/users')
     -Arrays of paths (['/users', '/customers'])
+
 - add nodemon to run file automatically
+
 - order of routes metters alot
+
 - add multiple route handler for single route
+
+- add middleware "It provides a way to add and reuse common functionality
+                     across your application's routes and endpoints."
+
+- Middleware can:
+    -Execute any code
+    -Modify request and response objects
+    -End the request-response cycle
+    -Call the next middleware in the stack
+    
+-When you call next(), the next middleware in the stack is executed.
+
+-If you don't call next(), the request-response cycle ends and no further middleware runs.
--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const express = require("express");
 const app = express();
 
 
-// we can add multiple routes handlers (middleware)  for  a single  route 
-
-app.get("/user/:userId/:name/:password", (req, res,next) => {
+// use of middleware
+app.get("/user", (req, res, next) => {
+    // we can add multiple routes handlers (middleware)  for  a single  route 
 
     res.send(
         console.log("response1!"),
@@ -16,32 +16,16 @@ app.get("/user/:userId/:name/:password", (req, res,next) => {
 
     )
 
-},
-    (req, res) => {
-        res.send("how are you ")
-    }
-
-)
-app.post("/user", (req, res) => {
-    res.send('post req executed')
+});
+app.use((req, res) => {
+    res.send('middle ware 2')
 })
 
-app.use('/hello/2', (req, res) => {
-    res.send('hello 2 will be print ')
-})
 
-// whenever we change the parameter we have to rerun the file 
-app.use('/hello', (req, res) => {
-    res.send('server is running theough nodemon');
-})
-// here is the sequence matters when you use the similar path you have to call this method on top of /hello path 
-// app.use('/hello/2', (req, res)=>{
-//     res.send('hello 2 will be print ')
-// })
-app.use('/', (req, res) => {
-    res.send('go to /hello page');
-})
-// to prevent file render the code for every change we use Node nodemon
+
+
+
+
 app.listen(55555, () => {
     console.log('express is running');
 })
